59337bcee7a195fd87346dc43312cf1ca7b87bb9
Yeaeun Han <91946024+YeaaeunHan@users.noreply.github.com>
2022-11-06 22:34:40 -0500

    add loader

--- components/Thankyou.js
+++ components/Thankyou.js
@@ -1,43 +1,84 @@
-import {useState} from 'react';
-import { generateAI } from '../pages/api/generate';
+import { useState } from "react";
+import { generateAI } from "../pages/api/generate";
 import {
-    FormControl,
-    FormLabel,
-    Input,
-    Center,
-    Button,
-    Box
-  } from '@chakra-ui/react';
-
+  FormControl,
+  FormLabel,
+  Input,
+  Center,
+  Button,
+  Box,
+} from "@chakra-ui/react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Text,
+  CircularProgress,
+} from "@chakra-ui/react";
 function Thankyou() {
-    const [data, setData] = useState('..awaiting data');
-    const handleSubmit=async (e)=> {
-        e.preventDefault();
-        const formData= new FormData(e.target);
-        let formDataObj=Object.fromEntries(formData.entries());
-
-        let prompt= 'Thank you email about '+ formDataObj.emailInput;
-        console.log(prompt)
-        generateAI(prompt).then((response)=>{
-            setData(response.data.choices[0].text)
-        }).catch((err)=>console.log(err));
-    }
+  const [data, setData] = useState("..awaiting data");
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    let formDataObj = Object.fromEntries(formData.entries());
+    setIsLoading(true);
+    let prompt = "Thank you email about " + formDataObj.emailInput;
+    console.log(prompt);
+    generateAI(prompt)
+      .then((response) => {
+        setData(response.data.choices[0].text);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="tweets">
-        <form onSubmit={handleSubmit}>
-            <FormControl mb="10">
-                <FormLabel>Thank you email about .. ?</FormLabel>
-                <Input type='text' name='emailInput' />
-            </FormControl>
-            <Center>
-                <Button colorScheme='teal' type="submit">Generate</Button>
-            </Center>
-        </form>
-        <h3 mt="10" as='b'>Response</h3>
-        <Box mt='2' borderWidth='1px' padding='5' borderRadius='lg'>
-            <p>{data}</p>
+      <form onSubmit={handleSubmit}>
+        <FormControl mb="10">
+          <FormLabel>Thank you email about .. ?</FormLabel>
+          <Input type="text" name="emailInput" />
+        </FormControl>
+        <Center>
+          <Button colorScheme="teal" type="submit">
+            Generate
+          </Button>
+          {isLoading && (
+            <CircularProgress isIndeterminate color="green.300" ml="10%" />
+          )}
+        </Center>
+      </form>
+      {data.length !== 0 && (
+        <Box
+          bg="#d4f2f5"
+          mt="6"
+          borderWidth="1px"
+          padding="5"
+          borderRadius="lg"
+        >
+          <TableContainer whiteSpace="normal">
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                  <Th>Generated Tweet</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>
+                    <Text wordBreak="break-word">{data}</Text>
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
         </Box>
+      )}
     </div>
-    );
+  );
 }
 export default Thankyou;
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,8 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import Tweets from '../components/Tweets';
-import { Center, VStack, Box, Flex } from '@chakra-ui/react'
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Center, VStack, Box, Flex, Text } from '@chakra-ui/react'
 import Thankyou from '../components/Thankyou';
-import Link from 'next/link';
 
 export default function Home() {
   const [which,setWhich]= useState(0);
@@ -22,37 +20,33 @@ export default function Home() {
       </Head>
 
       <main className={styles.main} >
-        <h1 className={styles.title} mt='20'>
-          AI Generator
-        </h1>
-        <Box mb='9' fontSize='xl'>Built with NextJS & OPEN AI</Box>
-
-        <Grid w='90%' h='50vh'
-          templateColumns='repeat(5, 1fr)'
-          gap='5'
-          color='blackAlpha.700'
-          fontWeight='bold'
-        >
-          <GridItem colSpan={1}  >
+          <Flex>
             <VStack
+            min-w='200px'
               spacing={4}
               align='start stretch' 
+              justify='left'
               bg='#d4f2f5'
-              h='100%'
+              h='100vh'
+              width='200px'
+              marginRight='3'
+              paddingTop='40'
             >
               <Center h='40px' align='center' justifyContent='center'>
-                <div onClick={() => {setWhich(0)}}>Tweets</div>
+                <div  onClick={() => {setWhich(0)}}><Text as='b'>Tweets</Text></div>
               </Center>
               <Center h='40px' align='center' >
-                <div onClick={() => {setWhich(1)}}>Thank you</div>
+                <div onClick={() => {setWhich(1)}}><Text as='b'>Thank you</Text></div>
               </Center>
             </VStack>
-          </GridItem>
-
-          <GridItem colSpan={4} >
+          <Box m='5' width='70%'>
+          <h1 className={styles.title} mt='20'>
+          AI Generator
+        </h1>
+        <Center mb='9' fontSize='xl'>Built with NextJS & OPEN AI</Center>
             {which === 0? <Tweets/>: <Thankyou/>}
-          </GridItem>
-        </Grid>
+        </Box>
+        </Flex>
       </main>
     </ChakraProvider>
   )
