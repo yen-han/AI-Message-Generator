0d536b87af83c9c62d1dbba26c2aeb1f7e2ad934
Yeaeun Han <yenhan.dev@gmail.com>
2023-01-15 21:57:36 -0500

    feat: apply pagination

--- components/SavedMessages.js
+++ components/SavedMessages.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import ReactPaginate from "react-paginate";
 import {
   Box,
   Table,
@@ -9,8 +11,19 @@ import {
   TableContainer,
   Checkbox,
 } from "@chakra-ui/react";
-
+import styles from "../styles/SavedMessages.module.scss";
 function SavedMessages() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [recordsPerPage] = useState(5);
+  const [messages, setMessages] = useState(["no messages"]);
+
+  const indexOfFirstRecord = currentPage * recordsPerPage;
+  const indexOfLastRecord = indexOfFirstRecord + recordsPerPage;
+  const currentRecords = messages.slice(indexOfFirstRecord, indexOfLastRecord);
+  const pageCount = Math.ceil(messages.length / recordsPerPage);
+  const changePage = ({ selected }) => {
+    setCurrentPage(selected);
+  };
   return (
     <Box bg="blue.50" mt="6" borderWidth="1px" padding="5" borderRadius="lg">
       <TableContainer whiteSpace="normal">
@@ -31,6 +44,17 @@ function SavedMessages() {
           </Tbody>
         </Table>
       </TableContainer>
+      <ReactPaginate
+        className={styles.pagination}
+        previousLabel={"<"}
+        nextLabel={">"}
+        pageRangeDisplayed={recordsPerPage}
+        marginPagesDisplayed={recordsPerPage}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        disabledClassName={"disabled"}
+        activeClassName={"active"}
+      />
     </Box>
   );
 }
