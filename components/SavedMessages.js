be3b4ed8caa03f4db07f379a6ff68f89485461cd
Yeaeun Han <yenhan.dev@gmail.com>
2023-01-19 21:46:56 -0500

    feat: add delete functionality with trash icon on savedMessages page

--- components/SavedMessages.js
+++ components/SavedMessages.js
@@ -11,10 +11,23 @@ import {
   Td,
   TableContainer,
   Checkbox,
+  Icon,
+} from "@chakra-ui/react";
+import {
+  Button,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
 } from "@chakra-ui/react";
 import styles from "../styles/SavedMessages.module.scss";
 import axios from "axios";
-
+import { BiTrash } from "react-icons/bi";
 function SavedMessages() {
   const [currentPage, setCurrentPage] = useState(0);
   const [recordsPerPage] = useState(5);
@@ -26,7 +39,7 @@ function SavedMessages() {
         setMessages(JSON.parse(JSON.stringify(response.data)));
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [messages]);
   const indexOfFirstRecord = currentPage * recordsPerPage;
   const indexOfLastRecord = indexOfFirstRecord + recordsPerPage;
   const currentRecords = messages.slice(indexOfFirstRecord, indexOfLastRecord);
@@ -34,6 +47,12 @@ function SavedMessages() {
   const changePage = ({ selected }) => {
     setCurrentPage(selected);
   };
+
+  function onDelete(message) {
+    // console.log(message);
+    axios.post("/api/deleteMessages", message);
+  }
+
   return (
     <Box bg="blue.50" mt="6" borderWidth="1px" padding="5" borderRadius="lg">
       <TableContainer whiteSpace="normal">
@@ -52,7 +71,43 @@ function SavedMessages() {
                   <Td>{message.topic}</Td>
                   <Td>{message.text} </Td>
                   <Td>
-                    <Checkbox alignItems="center" size="lg"></Checkbox>
+                    <Center>
+                      <Popover>
+                        {({ onClose }) => (
+                          <>
+                            <PopoverTrigger>
+                              <Button variant="outline">
+                                <Icon as={BiTrash} />
+                              </Button>
+                            </PopoverTrigger>
+
+                            <PopoverContent w="230px">
+                              <PopoverArrow />
+                              <PopoverHeader>
+                                Are you sure to delete?
+                              </PopoverHeader>
+                              <PopoverCloseButton />
+                              <PopoverBody>
+                                <Center>
+                                  <Button
+                                    colorScheme="blue"
+                                    mr={5}
+                                    onClick={(e) => {
+                                      onDelete(message);
+                                    }}
+                                  >
+                                    Yes
+                                  </Button>
+                                  <Button colorScheme="gray" onClick={onClose}>
+                                    No
+                                  </Button>
+                                </Center>
+                              </PopoverBody>
+                            </PopoverContent>
+                          </>
+                        )}
+                      </Popover>
+                    </Center>
                   </Td>
                 </Tr>
               );
